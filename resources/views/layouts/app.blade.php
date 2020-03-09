69b1e424e3c58e4f7f2276c6bbac290e0ab6abee
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-09 16:37:06 +0100

    made the logo small

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                The logo will be here
+                <small>The logo will be here</small>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
