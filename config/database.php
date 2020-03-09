df692280743b5b85ec63332fe50959d57251f78a
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-09 14:01:15 +0100

    added zenit as database name

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'Zenit'),
+            'database' => env('DB_DATABASE', 'zenit'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'mysql'),
             'unix_socket' => env('DB_SOCKET', ''),
