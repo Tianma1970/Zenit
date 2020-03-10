8ee6efb14e4172075c81be3a24ef28e2354d7f67
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-10 16:16:06 +0100

    added course titles to be shown as a list on home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,25 @@
 
 @section('content')
 <div class="container">
+    @include('partials/status')
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-3">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Courses</div>
+                <div class="card-body">
+                    <ul>
+                        @foreach($courses as $course)
+                        <li>{{ $course->title}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Welcome: {{ Auth::user()->name }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     You are logged in as <strong>{{ strtoupper(Auth::user()->type )}}</strong>!<br>
 
                     Member Page: <a href="{{ url('/')}}/memberOnlyPage">Member</a><br>
