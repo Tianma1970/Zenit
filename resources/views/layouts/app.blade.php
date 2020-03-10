91542b82993b7ac081f93d761bd73b31e25a1900
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-10 16:32:02 +0100

    added link to home.blade.php and if statement for Auth::user in general outside user type if statement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,15 +27,20 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <small>The logo will be here</small>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button>  --}}
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
-                        @if(Auth::user())
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home">Zenit</a>
+                        </li>
+
+                    @if(Auth::user())
+                        @if(Auth::user()->type === 'member')
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Personligt
