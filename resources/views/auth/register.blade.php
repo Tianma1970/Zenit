15b146af925a28e8adda55394900d0b8c841b581
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-10 09:43:52 +0100

    added select user type when register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,16 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="type" class="col-md-4 text-md-right control-label">User type</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="type" id="type">
+                                    <option value="member">Member</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
