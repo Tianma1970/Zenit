c420b757a6265b657e5229820ae2456e0644dbc1
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-11 14:46:45 +0100

    created file

---
+++ resources/views/news/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container">
+        <h1>Edit {{ $news->title }}</h1>
+
+        <form method="POST" action="/news/{{ $news->id }}" class="col-6">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="title">Titel</label>
+                <input type="title" id="title"
+                  name="title" required value="{{ old('title') ? old('title') : $news->title }}"
+                  placeholder="Title"
+                  class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="content">Content</label>
+                <input type="text-area" id="content"
+                  name="content" required value="{{ old('content') ? old('content') : $news->content }}"
+                  placeholder="Content"
+                  class="form-control">
+            </div>
+
+            <!--Submit-->
+            <div class="form-group">
+                <input type="submit" value="Edit News"
+                class="btn btn-success">
+            </div>
+
+        </form>
+    </div>
+
+@endsection
---
+++ resources/views/news/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container">
+    <div class="card">
+        <div class="card-title text-center">
+            <h1>{{ $news->title }}</h1>
+        </div>
+
+        <div class="card-text">
+            <p>{{ $news->content }}</p>
+        </div>
+
+    </div>
+    @if(Auth::user()->type === 'admin')
+        <a href="/news/{{ $news->id}}/edit" class="btn btn-success">Edit News</a>
+        <form method="post" action="/news/{{ $news->id }}">
+            @csrf
+            @method("DELETE")
+        <input type="submit" value="Delete {{$news->title }}" class="btn btn-danger mt-5"></input>
+        </form>
+    @endif
+</div>
+
+@endsection
