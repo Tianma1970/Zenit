8b8eb83c9d620821a19e772929a6cb8b674bef62
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-11 10:58:49 +0100

    created method show

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -61,7 +61,10 @@ class NewsController extends Controller
      */
     public function show(News $news)
     {
-        //
+
+        return view('/news/show',[
+            'news'  => $news
+        ]);
     }
 
     /**
