1ca177dfeb95a0002449db80ce9c3a5d96e71731
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-12 10:38:13 +0100

    added dropdown for uploading of profile picture

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,11 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/upload">
+                                        {{__('Upload your profile picture')}}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
