a40f4f1d66961ec2bab8878a427ad36e36d14671
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-13 17:30:00 +0100

    made values required

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -5,12 +5,13 @@
     <div class="container">
         <h1>Add some News</h1>
 
+        @include('partials/error')
         <form method="POST" action="/news" class="col-6">
             @csrf
             <div class="form-group">
                 <label for="title">Titel</label>
                 <input type="title" id="title"
-                  name="title" value="{{ old('title') }}"
+                  name="title" required value="{{ old('title') }}"
                   placeholder="Title"
                   class="form-control">
             </div>
@@ -18,7 +19,7 @@
             <div class="form-group">
                 <label for="author">Author</label>
                 <input type="author" id="author"
-                  name="author" value="{{ old('author') }}"
+                  name="author" required value="{{ old('author') }}"
                   placeholder="author"
                   class="form-control">
             </div>
@@ -26,7 +27,7 @@
             <div class="form-group">
                 <label for="content">Content</label>
                 <textarea id="content" placeholder="Content"
-                  name="content" value="{{ old('content') }}"
+                  name="content" required value="{{ old('content') }}"
                   class="form-control">
                 </textarea>
             </div>
