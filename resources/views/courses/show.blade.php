92d7d6823951ff06f72cea101aa20fa5666247dc
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-13 10:45:33 +0100

    added if statements for knowledge, competence and forms / minor fix

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -8,18 +8,24 @@
             <small>Kursen är på {{ $course->points }} YH poäng</small><br><hr>
             <h3>Syfte och mål</h3>
             <p>{{ $course->goal }}</p><br>
+            @if($course->knowledge)
             <h3>Kunskaper</h3>
             <p>{{ $course->knowledge }}</p><br>
+            @endif
             <h3>Färdigheter</h3>
             <p>{{ $course->skills }}</p><br>
+            @if($course->competence)
             <h3>Kompetenser</h3>
             <p>{{ $course->competence }}</p><br>
+            @endif
             <h3>Kursens innehåll</h3>
             <p>{{ $course->content }}</p><br>
+            @if($course->forms)
             <h3>Betygskriterier</h3>
             <p>{{ $course->forms }}</p><br>
+            @endif
             <h3>Rekommenderad litteratur och dokumentation</h3>
-            <p>{{ $course->lierature }}</p><br>
+            <p>{{ $course->literature }}</p><br>
             <a href="/home" class="btn btn-info">Back</a>
 
             @if(Auth::user()->type === 'member')
