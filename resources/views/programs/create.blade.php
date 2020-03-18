65af6ef336697fea30bf85ff0bdc4dddfaa03d58
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-18 16:46:40 +0100

    created file for adding program

---
+++ resources/views/programs/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create a Program</div>
+                    <div class="card-body">
+                        @include('partials/status')
+                        <div class="jumbotron">
+                            <form method="post" action="/programs">
+                            @csrf
+                            <!--Name-->
+                            <div class="form-group">
+                                <label for="Name">Name</label>
+                                <input type="name" id="name" name="name" placeholder="Namn" class="col-8" required value="{{ old('name')}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="year">Year</label>
+                                <input type="year" id="year" name="year" placeholder="År" class="col-8" required value="{{ old('year')}}">
+                            </div>
+
+                            <!--Submit-->
+                            <div class="form-group">
+                                <input type="submit" value="Spara utbildning" class="btn btn-success">
+                            </div>
+
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
