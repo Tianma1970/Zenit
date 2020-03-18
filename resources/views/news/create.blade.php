41bd22520356189af9524607e951266862e2e339
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-18 16:10:46 +0100

    added Bootstrap classes

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -2,43 +2,58 @@
 
 @section('content')
 
-    <div class="container">
-        <h1>Add some News</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Add some News</div>
+                @include('partials/error')
+                    <div class="card-body">
+                        <div class="jumbotron">
+                            <div class="card-title">
 
-        @include('partials/error')
-        <form method="POST" action="/news" class="col-6">
-            @csrf
-            <div class="form-group">
-                <label for="title">Titel</label>
-                <input type="title" id="title"
-                  name="title" required value="{{ old('title') }}"
-                  placeholder="Title"
-                  class="form-control">
+                            </div>
+                                    @include('partials/error')
+                                    <form method="POST" action="/news" class="col-6">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label for="title">Titel</label>
+                                            <input type="title" id="title"
+                                              name="title" required value="{{ old('title') }}"
+                                              placeholder="Title"
+                                              class="form-control">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="author">Author</label>
+                                            <input type="author" id="author"
+                                              name="author" required value="{{ old('author') }}"
+                                              placeholder="author"
+                                              class="form-control">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="content">Content</label>
+                                            <textarea id="content" placeholder="Content"
+                                              name="content" required value="{{ old('content') }}"
+                                              class="form-control">
+                                            </textarea>
+                                        </div>
+
+                                        <!--Submit-->
+                                        <div class="form-group">
+                                            <input type="submit" value="Add News"
+                                            class="btn btn-success">
+                                        </div>
+
+                                    </form>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div class="form-group">
-                <label for="author">Author</label>
-                <input type="author" id="author"
-                  name="author" required value="{{ old('author') }}"
-                  placeholder="author"
-                  class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="content">Content</label>
-                <textarea id="content" placeholder="Content"
-                  name="content" required value="{{ old('content') }}"
-                  class="form-control">
-                </textarea>
-            </div>
-
-            <!--Submit-->
-            <div class="form-group">
-                <input type="submit" value="Add News"
-                class="btn btn-success">
-            </div>
-
-        </form>
+        </div>
     </div>
+</div>
 
 @endsection
