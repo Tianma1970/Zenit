ea8de55719064b12535544f97e0acc4abc5c711a
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-19 11:17:00 +0100

    added Course and Program namespaces

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Program;
+use App\Course;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
