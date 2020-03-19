c040329f5d6837ca20d6077c6434f69e30bc9b70
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-19 12:03:02 +0100

    added show method with $projects to be sent to middlewre

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -66,7 +66,12 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        //dd('show');
+        $projects = Project::orderBy('content')->get();
+
+        return view('/middleware', [
+            'projects'  => $projects
+        ]);
     }
 
     /**
