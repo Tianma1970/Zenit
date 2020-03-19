6a689536869d887014e563e3ade1c65fdddac323
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-19 17:09:34 +0100

    added foreach on Users project

--- resources/views/middleware.blade.php
+++ resources/views/middleware.blade.php
@@ -4,12 +4,17 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ strtoupper($message)}} only page!</div>
+                    <div class="card-header text-center"> {{Auth::user()->name }}s Projects</div>
                         <div class="card-body">
                             <div class="jumbotron">
                                 <div class="card-title">
                                     <div class="title m-b-md">
-
+                                        @foreach(Auth::user()->projects as $project)
+                                            {{ $project->content}}
+                                            @if(Auth::user()->projects->count() > 1)
+                                                <hr>
+                                            @endif
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
