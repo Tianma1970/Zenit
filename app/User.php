6409b744515b42f3f2abc17e6d50fae9b40e081f
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-19 12:03:40 +0100

    tiny fix in User-Project relationship

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
 
     public function projects()
     {
-        return $this->hasMany(User::class);
+        return $this->hasMany(Project::class);
     }
 
     public function program()
