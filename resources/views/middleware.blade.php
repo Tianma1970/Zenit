24bada060473b01573a9b52eb00a514e8e6daed8
Tillmann Weimer <tillmann1970@gmail.com>
2020-03-24 10:25:37 +0100

    added if statement for user type member / added button for create comment

--- resources/views/middleware.blade.php
+++ resources/views/middleware.blade.php
@@ -4,17 +4,31 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
+                    @if(Auth::user()->type === 'member')
                     <div class="card-header text-center"> {{Auth::user()->name }}s Projects</div>
+                    @else
+                    <div class="card-header text-center">
+                        Incomming projects
+                    </div>
+                    @endif
                         <div class="card-body">
                             <div class="jumbotron">
                                 <div class="card-title">
                                     <div class="title m-b-md">
-                                        @foreach(Auth::user()->projects as $project)
-                                            {{ $project->content}}
-                                            @if(Auth::user()->projects->count() > 1)
+                                        @foreach($projects as $project)
+                                            @if(Auth::user()->type === 'admin')
+                                                <h1>{{ $project->title }}</h1>
+                                                {{ $project->content}}<br>
+                                                @if(Auth::user()->projects->count() > 1)
                                                 <hr>
+                                                @endif
+                                                @if(Auth::user()->type === 'admin')
+                                                <a href="/comments/create" class="btn btn-info">create a comment</a>
                                             @endif
+                                        @endif
                                         @endforeach
+
+
                                     </div>
                                 </div>
                             </div>
