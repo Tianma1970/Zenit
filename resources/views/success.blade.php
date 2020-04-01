866091b4f8e1c2128e89a03c1528a98560461349
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-01 12:20:52 +0200

    created foreach loop for all users projects to be shown

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+<h3>Finished Courses</h3>
+<ol>
+@foreach(Auth::user()->projects as $project)
+        {{-- @if($project->completed === 1) --}}
+        <li>{{ $project->title }}</li>
+        {{-- @endif --}}
 
+@endforeach
+</ol>
 
 @endsection
