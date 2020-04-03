580ba207e121fdda4db9400541f9953641220884
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-03 13:33:18 +0200

    som general style fixes

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts/app')
 
 @section('content')
-
-<div class="container">
-    <div class="card">
-        <div class="card-title text-center">
-            <h1>{{ $news->title }}</h1>
+    <div class="container">
+        <div class="card text-center">
+            <div class="card-header">
+                <h1>{{ $news->title }}</h1>
+            </div>
+            <div class="card-body">
+                <p>{{ $news->content }}</p>
+            </div>
+            <div class="card-footer">
+                <small>News created at <i>{{ $news->updated_at }}</i></small><br>
+            </div>
         </div>
-
-        <div class="card-text">
-            <p>{{ $news->content }}</p>
-        </div>
-
+                @if(Auth::user()->type === 'admin')
+                <div class="row justify-content-around mt-5">
+                    <a href="/news/{{ $news->id}}/edit" class="btn btn-info">{{__('Edit:')}}&nbsp;{{$news->title}}</a>
+                    <form method="post" action="/news/{{ $news->id }}">
+                        @csrf
+                        @method("DELETE")
+                        <input type="submit" class="btn btn-danger" value="Delete {{$news->title }}"></input>
+                    </form>
+                </div>
+                @endif
     </div>
-    @if(Auth::user()->type === 'admin')
-        <a href="/news/{{ $news->id}}/edit" class="btn btn-success">{{__('Edit:')}}&nbsp;{{$news->title}}</a>
-        <form method="post" action="/news/{{ $news->id }}">
-            @csrf
-            @method("DELETE")
-        <input type="submit" value="Delete {{$news->title }}" class="btn btn-danger mt-5"></input>
-        </form>
-    @endif
-</div>
+
+
 
 @endsection
