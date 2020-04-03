15e165516d890826738c546dcc086f1546fa8393
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-03 14:01:31 +0200

    added card-header class

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -6,7 +6,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{__('Complete Project:')}}&nbsp;{{$project->title}}</div>
+                <div class="card-header text-center">
+                    <h3>{{__('Complete ')}}&nbsp;{{$project->title}}</h3>
+                </div>
                     <div class="card-body">
                         @include('partials/status')
                         <div class="jumbotron">
