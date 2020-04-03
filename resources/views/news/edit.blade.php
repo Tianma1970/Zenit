01a05714c6d79bf9bf556709bb4375ff5621e561
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-03 14:01:54 +0200

    general style fixes

--- resources/views/news/edit.blade.php
+++ resources/views/news/edit.blade.php
@@ -3,42 +3,53 @@
 @section('content')
 
 <div class="container">
-        <h1>Edit: {{ $news->title }}</h1>
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h3>Edit {{ $news->title }}</h3>
+                </div>
+                <form method="POST" action="/news/{{ $news->id }}">
+                @csrf
+                @method('PUT')
+                    <div class="card-body">
+                        <div class="jumbotron">
 
-        <form method="POST" action="/news/{{ $news->id }}" class="col-6">
-            @csrf
-            @method('PUT')
-            <div class="form-group">
-                <label for="title">{{__('Title')}}</label>
-                <input type="title" id="title"
-                  name="title" required value="{{ old('title') ? old('title') : $news->title }}"
-                  placeholder="Title"
-                  class="form-control">
+                        <!--Title-->
+                        <div class="form-group">
+                            <label for="title">{{__('Title')}}</label>
+                            <input type="title" id="title"
+                            name="title" required value="{{ old('title') ? old('title') : $news->title }}"
+                            placeholder="Title" class="form-control">
+                        </div>
+
+                        <!--Author-->
+                        <div class="form-group">
+                            <label for="author">{{__('Author')}}</label>
+                            <input type="author" id="author"
+                            name="author" required value="{{ old('author') ? old('author') : $news->author }}"
+                            placeholder="Author"
+                            class="form-control">
+                        </div>
+
+                        <!--Content-->
+                        <div class="form-group">
+                            <label for="content">{{__('Content')}}</label>
+                            <input type="text-area" id="content"
+                            name="content" required value="{{ old('content') ? old('content') : $news->content }}"
+                            placeholder="Content"
+                            class="form-control">
+                        </div>
+
+                        <!--Submit-->
+                        <div class="form-group">
+                            <input type="submit" value="Edit News"
+                            class="btn btn-success">
+                        </div>
+                    </div>
+                </form>
             </div>
-
-            <div class="form-group">
-                <label for="author">{{__('Author')}}</label>
-                <input type="author" id="author"
-                  name="author" required value="{{ old('author') ? old('author') : $news->author }}"
-                  placeholder="Author"
-                  class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="content">{{__('Content')}}</label>
-                <input type="text-area" id="content"
-                  name="content" required value="{{ old('content') ? old('content') : $news->content }}"
-                  placeholder="Content"
-                  class="form-control">
-            </div>
-
-            <!--Submit-->
-            <div class="form-group">
-                <input type="submit" value="Edit News"
-                class="btn btn-success">
-            </div>
-
-        </form>
+        </div>
     </div>
-
+</div>
 @endsection
