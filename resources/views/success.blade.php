cb057feeda4f56020f25a9eadf6b314855d8b8a2
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-08 11:12:01 +0200

    layout fix created loop

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -2,14 +2,21 @@
 
 @section('content')
 
-<h3>{{__('Finished Courses')}}</h3>
-<ol>
-@foreach(Auth::user()->projects as $project)
-        @if($project->completed === 1)
-        <li>{{ $project->title }}</li>
-        @endif
+<div class="row justify-content-center">
+    <div class="jumbotron col-8">
+        <h4 class="display-4">{{ __('Finished Projects') }}</h4>
+        <ol>
+        @foreach(Auth::user()->projects as $project)
+            @if($project->comments)
+                @if($project->completed === 0)
 
-@endforeach
-</ol>
+                    <li>{{ $project->title }}</li>
 
+                @endif
+            @endif
+        @endforeach
+        </ol>
+        <a href="/home" class="btn btn-info">{{ __('Back')}}</a>
+    </div>
+</div>
 @endsection
