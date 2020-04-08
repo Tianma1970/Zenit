27c0d3c06902246a0f9f077212268c1872e2d0de
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-08 11:06:33 +0200

    cleaned up code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 
 
 <div class="row justify-content-center">
-<div class="jumbotron col-8">
-  <h4 class="display-4">{{ __('You are signed out') }}</h4>
+    <div class="jumbotron col-8">
+        <h4 class="display-4">{{ __('You are signed out') }}</h4>
 
-  <hr class="my-4">
+        <hr class="my-4">
 
-  <a class="btn btn-info btn-lg" href="/login" role="button">{{ __('Log in again') }}</a>
-</div>
+        <a class="btn btn-info btn-lg" href="/login" role="button">{{ __('Log in again') }}</a>
+    </div>
 </div>
 
 
