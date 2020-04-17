f34b1302fefeae8dedb8b95755c4bc9d6b5444b1
Tillmann Weimer <tillmann1970@gmail.com>
2020-04-17 13:41:46 +0200

    made link translateable from english

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                         @if(Auth::user()->type === 'member')
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Personligt
+                            {{ __('Private')}}
                             </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="/success">{{__('Your success')}}</a>
